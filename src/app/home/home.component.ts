94eb71d984459006cca9b2060e8963e90367671d
mjalte <m.j.altebrando+github@gmail.com>
2020-12-09 09:28:01 -0500

    1 file modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 /**
  * In this project, let's do the following:
@@ -7,38 +7,40 @@ import { HttpClient } from '@angular/common/http';
  *    a Camera (optional), and/or Page
  * 2. Make it so that I can click into a Rover, see it's details, and
  *    filter its photos by earth day/camera/page
- * 3. From a Rover Page, I can click into a Camera, see the camera details, 
+ * 3. From a Rover Page, I can click into a Camera, see the camera details,
  *    and filter its photos by earth day/page
  * 4. From Camera Page, click back to Rover Page, or Home Page
  * 5. From Rover Page, click back to Home Page
  */
 
 const defaultParams = {
-  api_key: 'NmEfVgBODcenE6iJITMp7z29XB7yLUvwkmEOe0eI', // Get your own key and put it here
+  api_key: "gEaKjqjtiTY1G3zsnpZnJNnhQIdj4fdDbDHhOARt" // My API Key
 };
 
-if(defaultParams.api_key === 'DEMO_KEY') console.warn('Go to https://api.nasa.gov and get your own API_KEY. It\'s easy!');
+if (defaultParams.api_key === "DEMO_KEY")
+  console.warn(
+    "Go to https://api.nasa.gov and get your own API_KEY. It's easy!"
+  );
 
-const roversApi = 'https://api.nasa.gov/mars-photos/api/v1/rovers';
-const photosApi = (roverName) => `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos`;
+const roversApi = "https://api.nasa.gov/mars-photos/api/v1/rovers";
+const photosApi = roverName =>
+  `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos`;
 
 // Possible Params: earth_day, camera, page
 
 @Component({
-  selector: 'rc-home',
-  templateUrl: './home.component.html',
-  styleUrls: [ './home.component.css' ]
+  selector: "rc-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
-export class HomeComponent  {
-  rovers$ = this.http.get(roversApi, {params: defaultParams});
-  data$ = this.http.get(photosApi('curiosity'), {
+export class HomeComponent {
+  rovers$ = this.http.get(roversApi, { params: defaultParams });
+  data$ = this.http.get(photosApi("curiosity"), {
     params: {
       ...defaultParams,
-      earth_date: '2019-09-01'
+      earth_date: "2019-09-01"
     }
-  })
+  });
 
-  constructor(private http: HttpClient) {
-
-  }
+  constructor(private http: HttpClient) {}
 }
